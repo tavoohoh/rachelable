5b4ce51eec9e49947ac3bd0f57ca3f3760a65ab9
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-04-27 17:02:33 -0500

    inverted nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationArrayModel } from '@mode/index';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'ray-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.sass']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  public invertedColors: boolean;
   public links: NavigationArrayModel = [
     {
       path: '/home',
@@ -21,5 +23,25 @@ export class NavbarComponent {
       text: 'PAGES.CONTACT.TITLE'
     }
   ];
-  
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.invertedColors = this.setLayout(this.router.url.split('/').reverse()[0]);
+
+    this.router.events.subscribe((navigateEvent: any) => {
+      if (navigateEvent instanceof NavigationEnd) {
+        this.invertedColors = this.setLayout(navigateEvent.url.split('/').reverse()[0]);
+      }
+    });
+  }
+
+  private setLayout(currentUrl: string): boolean {
+    if (currentUrl === 'contact') {
+      return true;
+    }
+
+    this.invertedColors = false;
+  }
+
 }
