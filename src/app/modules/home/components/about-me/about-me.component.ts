3c9cf3f56b3d74715e5bf57f1500aa22e4f56a6f
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2021-05-21 19:41:18 -0500

    lint files

--- src/app/modules/home/components/about-me/about-me.component.ts
+++ src/app/modules/home/components/about-me/about-me.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ray-home-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
 })
-export class AboutMeHomeComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AboutMeHomeComponent {
 }
