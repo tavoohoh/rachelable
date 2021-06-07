129f2c8f81de858e5cbe36fe3f497c8c5e0850fa
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2021-06-07 14:20:30 -0500

    update cover image and loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AppService } from '@app/app.service';
 @Component({
   selector: 'ray-root',
   template: `
-    <ray-loader *ngIf="loading"></ray-loader>
+    <ray-loader *ngIf="showLoader"></ray-loader>
     <div class="main" *ngIf="!loading">
       <router-outlet></router-outlet>
       <ray-footer></ray-footer>
@@ -14,12 +14,14 @@ import { AppService } from '@app/app.service';
 })
 export class AppComponent implements OnInit {
   public loading = true;
+  public showLoader = true;
 
   constructor(private service: AppService) {}
 
   async ngOnInit(): Promise<void> {
     await this.service.initApp();
+    this.loading = false
 
-    setTimeout(() => (this.loading = false), 1000);
+    setTimeout(() => (this.showLoader = false), 1300);
   }
 }
